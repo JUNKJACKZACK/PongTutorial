650c5a5e04a90560970dbada978f939c13262cec
Zachary Donnelly <zacharydonnelly_IT@protonmail.com>
2022-10-07 21:52:01 -0600

    Initial Posting of Repository

---
+++ Pong.py
@@ -0,0 +1,24 @@
+#Followed tutorial code from FreeCodeCamp on YouTube
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+#Paddle A
+paddle_a = turtle.Turtle()
+
+
+
+#Paddle B
+
+#Ball
+
+
+#Main game loop
+while True:
+    wn.update()
+    
